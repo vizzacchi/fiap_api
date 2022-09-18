2adb7f6b3e1f689751af01bb0b454eb0e8ff0aff
Douglas Vizzacchi <douglas@vizzacchi.com.br>
2022-09-17 22:16:48 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const app = express();
+var saida;
 
 const port = process.env.PORT || 3000;
 
 app.get("/", function (req, res){
-    res.send("Douglas é FODA!!!!");
+    if (!navigator.geolocation)
+        return null;
+    navigator.geolocation.getCurrentPosition((pos) => {
+        latUser = document.getElementById("lat").innerText = pos.coords.latitude;
+        lonUser = document.getElementById("lon").innerText = pos.coords.longitude;
+        saida =  "{lat: "+latUser+",lon: "+lonUser+"}";
+    })
+
+
+
+    res.send(saida);
 });
 
 app.listen(port, () => {

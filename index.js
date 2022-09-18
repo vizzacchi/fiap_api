d80560c23170f23f4c52d8e95e2d632eca12b171
Douglas Vizzacchi <douglas@vizzacchi.com.br>
2022-09-17 21:53:16 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const app = express();
 
-const port = process.env.Port || 3000;
+const port = process.env.PORT || 3000;
 
 app.get("/", function (req, res){
     res.send("Douglas é FODA!!!!");
 });
 
 app.listen(port, () => {
-    console.info("Primeira API FIAP rodando em http://localhost");
+    console.info("Primeira API FIAP rodando!!!");
 });
